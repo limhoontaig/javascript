3a4ab0a1479b844108e6153ff77db7350fa262a6
limhoontaig <limhoontaig@gmail.com>
2021-06-27 07:27:21 +0900

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+
 console.log('Hello World!');
+'use strict';
+let a;
+a = 6;
+console.log(a)
+// 2. Variable
+// let (added in ES6)
+let globalName = 'global name';
+{
+  let name = 'ellie';
+  console.log(name);
+  name = 'hello';
+  console.log(name);
+  console.log(globalName);
+}
+console.log(name);
+console.log(globalName);
+
+// var splash (dont ever use it)
+// var hoisting(move declaration from bottom to top)
+// has no block scope
+{
+
+  console.log(age)
+  age = 4;
+  var age;
+}
+console.log(age)
