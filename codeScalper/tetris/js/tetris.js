4ae2a6802470104976511a71fad08f3f910bbd17
limhoontaig <limhoontaig@gmail.com>
2021-07-06 07:10:37 +0900

    first step for tetris

---
+++ codeScalper/tetris/js/tetris.js
@@ -0,0 +1,27 @@
+// DOM
+const playground = document.querySelector('.playground > ul');
+
+// Setting
+const GAME_ROWS = 20;
+const GAME_COLS = 10;
+
+init()
+
+// Functions
+function init() {
+  for (let i = 0; i <GAME_ROWS; i++) {
+    prependNewLine()
+}
+
+}
+
+FUNCTION prependNewLine() {
+  const li = document.createElement('li');
+  const ul = document.createElement('ul');
+  for (let j=0; j < GAME_COLS; j++) {
+    const matrix = document.createElement('li');
+    ul.prepend(matrix)
+  }
+  li.prepend(ul)
+  playground.prepend(li)
+}
