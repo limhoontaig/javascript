49ae02614f8ef8a01c620738925a014ddeace1c4
limhoontaig <limhoontaig@gmail.com>
2021-07-06 17:10:26 +0900

    Interim part of tetris

--- codeScalper/tetris/js/tetris.js
+++ codeScalper/tetris/js/tetris.js
@@ -5,17 +5,41 @@ const playground = document.querySelector('.playground > ul');
 const GAME_ROWS = 20;
 const GAME_COLS = 10;
 
+// variables
+let score =0;
+let duration = 500;
+let downInterval;
+let tempMovingItem;
+
+const BLOCKS = {
+  tree: [
+    [[0,0],[0,1],[1,0],[1,1]],
+    [],
+    [],
+    [],
+  ]
+}
+
+const movingItem = {
+  type: 'tree',
+  direction: 0,
+  top: 0,
+  left: 0,
+};
+
 init()
 
 // Functions
 function init() {
+  tempMovingItem = { ...movingItem};
   for (let i = 0; i <GAME_ROWS; i++) {
     prependNewLine()
-}
+  }
+  renderBlocks()
 
 }
 
-FUNCTION prependNewLine() {
+function prependNewLine() {
   const li = document.createElement('li');
   const ul = document.createElement('ul');
   for (let j=0; j < GAME_COLS; j++) {
@@ -24,4 +48,14 @@ FUNCTION prependNewLine() {
   }
   li.prepend(ul)
   playground.prepend(li)
+}
+
+function renderBlocks(){
+  const { type, direction, top, left } = tempMovingItem;
+  BLOCKS[type][direction].forEach(block => {
+    const x = block[0];
+    const y = block[1];
+    console.log({ playground })
+    const target = playground.childNodes;
+  })
 }
