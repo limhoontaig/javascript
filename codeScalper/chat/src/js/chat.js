22f9e52dbd4990a9ce66acd2e1e977682d5836c7
limhoontaig <limhoontaig@gmail.com>
2021-07-11 09:42:30 +0900

    1'st step completion

--- codeScalper/chat/src/js/chat.js
+++ codeScalper/chat/src/js/chat.js
@@ -30,13 +30,13 @@ function LiModel(name, msg, time) {
     const li = document.createElement('li');
     li.classList.add(nickname.value === this.name ? "sent": "received")
     const dom = `<span class="profile">
-      <span class="user">${tjis.name}</span>
-      <img class="image" src="https://placeimg.com/50/50/any" alt="any">
-    </span>
-    <span class="message">${this.msg}</span>
-    <span class="time">${this.time}</span>`;
-    li.innerHTML = dom;
-    chatList.appendChild(li);
+    <span class="user">${this.name}</span>
+    <img class="image" src="https://placeimg.com/50/50/any" alt="any">
+  </span>
+  <span class="message">${this.msg}</span>
+  <span class="time">${this.time}</span>`;
+  li.innerHTML = dom;
+  chatList.appendChild(li);
   }
 }
 
