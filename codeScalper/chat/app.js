74f22651b794198bdffe66667f129103082c1278
limhoontaig <limhoontaig@gmail.com>
2021-07-11 10:21:04 +0900

    view time

--- codeScalper/chat/app.js
+++ codeScalper/chat/app.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require('path');
 const server = http.createServer(app);
 const socketIO = require('socket.io');
+const moment = require("moment");
 
 const io = socketIO(server);
 
@@ -14,8 +15,12 @@ const PORT = process.env.PORT || 5000;
 // app.listen(PORT, Command)
 io.on("connection", (socket) => {
     socket.on('chatting', (data) => {
-      console.log(data)
-      io.emit('chatting', data)
+      const { name, msg } = data;
+      io.emit('chatting', {
+        name,
+        msg,
+        time: moment(new Date()).format("hh:mm A")
+      })
   })
 })
 
