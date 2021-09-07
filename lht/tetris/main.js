6bd5ec6be056803bdb9e0a5a9633536f5dadd93d
= <=>
2021-09-07 17:45:41 +0900

    interim

--- lht/tetris/main.js
+++ lht/tetris/main.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext('2D');
 
 // Calculate the canvas size using constant
 ctx.canvas.width = COLS * BLOCK_SIZE;
-ctx.canvas.height = COLS * BLOCK_SIZE;
+ctx.canvas.height = ROWS * BLOCK_SIZE;
 
 // Change the block size using scale method
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
